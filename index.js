672c5cbec41ee331857c7efe96502c1c280f9906
Clive <cliveong1999@gmail.com>
2022-07-07 21:37:15 +0800

    Test anilist api

---
+++ index.js
@@ -0,0 +1,68 @@
+// Here we define our query as a multi-line string
+// Storing it in a separate .graphql/.gql file is also possible
+var query = `
+query ($id: Int, $page: Int, $perPage: Int, $search: String) {
+  Page (page: $page, perPage: $perPage) {
+    pageInfo {
+      total
+      currentPage
+      lastPage
+      hasNextPage
+      perPage
+    }
+    media (id: $id, search: $search, type: ANIME) {
+      id
+      title {
+        romaji
+        english
+        native
+      }
+      coverImage {
+          extraLarge
+      }
+      type
+    }
+  }
+}
+`;
+
+// Define our query variables and values that will be used in the query request
+var variables = {
+    search: "tenki no",
+    page: 1,
+    perPage: 10,
+};
+
+// Define the config we'll need for our Api request
+var url = 'https://graphql.anilist.co',
+    options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+        },
+        body: JSON.stringify({
+            query: query,
+            variables: variables
+        })
+    };
+
+// Make the HTTP Api request
+fetch(url, options).then(handleResponse)
+                   .then(handleData)
+                   .catch(handleError);
+
+function handleResponse(response) {
+    return response.json().then(function (json) {
+        return response.ok ? json : Promise.reject(json);
+    });
+}
+
+function handleData(data) {
+    console.log(data);
+}
+
+function handleError(error) {
+    alert('Error, check console');
+    console.error(error);
+}
