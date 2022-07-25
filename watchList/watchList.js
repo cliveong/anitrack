244082e47cabac1dfe43ff8cd078a7bc750a0669
Clive <cliveong1999@gmail.com>
2022-07-25 18:10:24 +0800

    Add coloring to selected tabs for higher visibility

--- watchList/watchList.js
+++ watchList/watchList.js
@@ -40,19 +40,54 @@ const renderWatchList = (items) => {
 
 const tabs = () => {
     const toFinishBtn = document.querySelector(".toFinish");
+    const finishedBtn = document.querySelector(".completed");
+    const bothBtn = document.querySelector(".all");
+    const largeText = "1.8rem";
+    const normalText = "1.3rem";
+    const largePadding = "10px 10px";
+    const normalPadding = "16px 16px";
+    const largeBgColor = "gray";
+    const normBgColor = "#2C2C2C";
+
     toFinishBtn.addEventListener("click", () => {
         renderWatchList(unfinished);
+        toFinishBtn.style.fontSize = largeText;
+        toFinishBtn.style.padding = largePadding;
+        toFinishBtn.style.backgroundColor = largeBgColor;
+        finishedBtn.style.fontSize = normalText;
+        finishedBtn.style.padding = normalPadding;
+        finishedBtn.style.backgroundColor = normBgColor;
+        bothBtn.style.fontSize = normalText;
+        bothBtn.style.padding = normalPadding;
+        bothBtn.style.backgroundColor = normBgColor;
+
     });
 
-    const finishedBtn = document.querySelector(".completed");
     finishedBtn.addEventListener("click", () => {
         renderWatchList([]);
+        toFinishBtn.style.fontSize = normalText;
+        toFinishBtn.style.padding = normalPadding;
+        toFinishBtn.style.backgroundColor = normBgColor;
+        finishedBtn.style.fontSize = largeText;
+        finishedBtn.style.padding = largePadding;
+        finishedBtn.style.backgroundColor = largeBgColor;
+        bothBtn.style.fontSize = normalText;
+        bothBtn.style.padding = normalPadding;
+        bothBtn.style.backgroundColor = normBgColor;
     });
 
-    const bothBtn = document.querySelector(".all");
     bothBtn.addEventListener("click", () => {
         const tempArrayAll = unfinished.concat(finished);
         renderWatchList(tempArrayAll);
+        toFinishBtn.style.fontSize = normalText;
+        toFinishBtn.style.padding = normalPadding;
+        toFinishBtn.style.backgroundColor = normBgColor;
+        finishedBtn.style.fontSize = normalText;
+        finishedBtn.style.padding = normalPadding;
+        finishedBtn.style.backgroundColor = normBgColor;
+        bothBtn.style.fontSize = largeText;
+        bothBtn.style.padding = largePadding;
+        bothBtn.style.backgroundColor = largeBgColor;
     });
 }
 
